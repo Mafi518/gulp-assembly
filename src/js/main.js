bcb6a7afd61812ae0afc1903d050bdb4d994c30c
Andrew_Dev <xolluej@list.ru>
2020-10-06 22:11:12 +0400

    modified assembly 06.10.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-// 111111111111111111
+const disableScroll = function() {
+    let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px'
+    document.body.classList.toggle('scroll-lock')
+    document.body.style.paddingRight = paddingOffset
+}
+
+const burger = document.querySelector('.menu-icon').addEventListener('click', function() {
+    const menu = document.querySelector('.menu')
+    this.classList.toggle('menu-icon__active')
+    menu.classList.toggle('menu__active')
+
+    let links = document.querySelectorAll('.menu__link').forEach(element => {
+        element.onclick = () => {
+            menu.classList.toggle('menu__active')
+            this.classList.toggle('menu-icon__active')
+        }
+    })
+})
